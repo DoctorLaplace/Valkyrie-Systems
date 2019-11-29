dad344a76cc86bf19b1fd18530e597df910e07e4
Doctor Laplace <58324905+DoctorLaplace@users.noreply.github.com>
2019-11-29 01:29:58 -0800

    Create source.cpp
    The placeholder.

---
+++ C++ Code/source.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <sstream>
+#include <SFML/Graphics.hpp>
+#include <math.h>
+#include <string>
+#include <list>
+#include <vector>
+#include <cstdlib>
+#include <SFML/Network.hpp>
+using namespace std;
+
+int main()
+{
+		return 0;
+}
